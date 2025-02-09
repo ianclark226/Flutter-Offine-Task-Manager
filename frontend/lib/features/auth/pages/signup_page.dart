aaa5edf89feb9b6ea140176c940c677b6dd37939
Ian Clark <ianclark226@gmail.com>
2025-02-09 17:05:06 -0500

    login and signup ui complete

---
+++ frontend/lib/features/auth/pages/signup_page.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/features/auth/pages/login_page.dart';
+
+class SignupPage extends StatefulWidget {
+  static route() => MaterialPageRoute(builder: 
+   (context) => const SignupPage(),
+  );
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    nameController.dispose();
+    super.dispose();
+  }
+
+  void signUpUser() {
+    if(formKey.currentState!.validate()) {
+
+    }
+  }
+
+  @override
+Widget build(BuildContext context) {
+  return Scaffold(
+    body: Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Form(  // Wrap the Column with Form
+        key: formKey,  // Assign formKey here
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Sign up.",
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30),
+            TextFormField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                hintText: 'Name',
+              ),
+              validator: (value) {
+                if (value == null || value.trim().isEmpty) {
+                  return "Name field cannot be empty!";
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 15),
+            TextFormField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                hintText: 'Email',
+              ),
+              validator: (value) {
+                if (value == null || value.trim().isEmpty || !value.trim().contains("@")) {
+                  return "Email field is invalid!";
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 15),
+            TextFormField(
+              controller: passwordController,
+              decoration: const InputDecoration(
+                hintText: 'Password',
+              ),
+              validator: (value) {
+                if (value == null || value.trim().isEmpty || value.trim().length <= 6) {
+                  return "Password field is invalid!";
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: signUpUser,
+              child: const Text(
+                'SIGN UP',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(LoginPage.route());
+              },
+            
+            child: RichText(
+              text: TextSpan(
+                text: 'Already have an account? ',
+                style: Theme.of(context).textTheme.titleMedium,
+                children: const [
+                   TextSpan(
+                    text: 'Login',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+}
